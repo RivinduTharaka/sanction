0ccb235d90271ff8a89f2fc8119d35dffa2a8369
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2025-03-16 00:22:30 +0530

    cmt

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Settings from './SettingsPage/Settings';
+
+function App() {
+  return (
+    <div>
+<Settings />
+    </div>
+  );
+}
+
+export default App;
